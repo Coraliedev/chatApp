82999dde6579936f4d165f05f637cd16d2251e94
CoralieDev <coralie.acquaert@gmail.com>
2023-09-17 11:24:52 +0200

    (backend) configure the app

--- server/app.js
+++ server/app.js
@@ -1,4 +1,39 @@
+'use strict'
+
 const express = require('express')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongosanitize = require('express-mongo-sanitize')
+const morgan = require('morgan')
+const xss = require('xss-clean')
+const cors = require('cors')
+
+
 const app = express()
+
+app.use(express.json({ limit: '10kb' }))
+app.use(express.urlencoded({ extended: true}))
+
+app.use(helmet())
+app.use(morgan('dev'))
+
+const limited = rateLimit({
+  max: 3000,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour'
+})
+
+app.use(limited)
+
+app.use(mongosanitize())
+app.use(xss())
+
+app.use(cors({
+  origin: '*',
+  methods: 'GET,PUT,POST,DELETE',
+  credentials: true,
+}))
+
+
 
 module.exports = app
