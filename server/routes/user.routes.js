ac0acae8bf2e51546dd6edb3c9afbe3904b0b387
CoralieDev <coralie.acquaert@gmail.com>
2023-09-18 17:30:01 +0200

    (backend) feat[user]: create route for get friends request

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -6,5 +6,6 @@ const { verifyToken } = require('../middleware/auth.middleware')
 
 router.get('/', verifyToken, userController.getUserInfo)
 router.get('/users', verifyToken, userController.getUsers)
+router.get('/friends', verifyToken, userController.getFriends)
 
 module.exports = router
